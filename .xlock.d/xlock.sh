d3ed5d21a3c9eb88637a862f21c82a0769a41c03
me@eskaton.ch <me@eskaton.ch>
2022-09-07 23:49:49 +0200

    Initial commit

---
+++ .xlock.d/xlock.sh
@@ -0,0 +1,18 @@
+xlock \
+    -mode image \
+    -username 'Username: ' \
+    -password 'Password:' \
+    -planfontset "-*-courier-bold-r-*-*-34-*-*-*-*-*-*-*" \
+    -fontset     "-*-courier-bold-r-*-*-34-*-*-*-*-*-*-*" \
+    -info ' ' \
+    -background gray10 \
+    -foreground gray80 \
+    -dpmsoff 120 \
+    -echokeys \
+    -echokey '*' \
+    -erasemode no_fade \
+    -bitmap ~/.xlock.d/xlock.xpm \
+    -ncolors 128 \
+    -icongeometry 128x128 \
+    -count 7 \
+    +description
